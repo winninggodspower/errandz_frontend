eed2898ef46bd5ad9522383fa0b54246a6e5c17e
winning godspower <winninggodspower@gmail.com>
2023-01-22 00:30:39 +0100

    edited navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import errandLogo from '../images/errandz-logo.svg';
-import './navbar.css'
+import './navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -7,14 +7,14 @@ function Navbar() {
         <nav className="navbar navbar-expand-md navbar-dark vw-100 navbar-fixed" aria-label="Seventh navbar example">
             <div className="container">
                 <Link className="navbar-brand" to={'/'}>
-                    <img src={errandLogo} alt="Logo" width="134px" height="44.6" className="d-inline-block align-text-top" />
+                    <img src={errandLogo} alt="Logo"  className="d-inline-block align-text-top" />
                 </Link>
                 <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleXxl" aria-controls="navbarsExampleXxl" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarsExampleXxl">
-                    <ul className="navbar-nav ms-auto mb-2 mb-xl-0">
+                    <ul className="navbar-nav ms-auto text-center">
                         <li className="nav-item ms-4">
                             <Link to="/" className="nav-link active">Home</Link>
                         </li>

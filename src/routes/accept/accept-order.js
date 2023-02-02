c1748981dc0da40083f6970f655a9e15b8ae97d4
uzochukwuV <vic.ezealor@gmail.com>
2023-02-02 03:09:53 +0100

    order page

--- src/routes/accept/accept-order.js
+++ src/routes/accept/accept-order.js
@@ -3,15 +3,15 @@
 function AcceptOrder(){
     return <>
     
-        <div className="  mx-auto d-flex">
+        <div className="  mx-auto d-flex vh-100 align-items-center justify-content-center">
 
-            <div className="mx-auto order-container bg-white position-absolute" >
+            <div className="mx-auto order-container bg-white position-absolute p-3" >
                 <div>
                     <p className="mx-auto">Recieved your order</p>
                 </div>
                 <div className="d-flex justify-content-around">
-                    <button type="submit" className="bg-dark text-white py-2">yes</button>
-                    <button type="submit" className="bg-dark text-white py-2">No</button>
+                    <button type="submit" className="bg-dark text-white px-2">yes</button>
+                    <button type="submit" className="bg-dark text-white px-2">No</button>
                 </div>
             </div>
         </div>
